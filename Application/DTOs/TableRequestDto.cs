5ba51aedb2cd663bffca56d708c764723c890332
trevortsui007-explorer <trevortsui007@gmail.com>
2026-03-26 10:50:57 +0800

    feat(api): add GetTableFields endpoint to retrieve metadata

--- Application/DTOs/TableRequestDto.cs
+++ Application/DTOs/TableRequestDto.cs
@@ -19,4 +19,15 @@ namespace DT_DataAcquisitionSystem.Application.DTOs
         public bool AllowNull { get; set; } = true;
         public int? MaxLength { get; set; }
     }
+
+    public class TableColumnInfo
+    {
+        public string ColumnName { get; set; }
+        public string DataType { get; set; }
+        public bool AllowDBNull { get; set; }
+        public bool IsIdentity { get; set; }
+        public bool IsPrimaryKey { get; set; }
+        public int MaxLength { get; set; }
+        public string DefaultValue { get; set; }
+    }
 }
